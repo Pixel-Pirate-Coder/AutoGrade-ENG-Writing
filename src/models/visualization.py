9983193f7342770230d032425eb1123a7e416cf1
MaxAndreyko <max_andreyko@mail.ru>
2024-06-25 16:23:56 +0300

    chore(visualization.py): change input parameters' names from graphviz to torchviz

--- src/models/visualization.py
+++ src/models/visualization.py
@@ -6,9 +6,9 @@ def plot_bert_architecture(model, max_size: int = 512,
                             min_index: int = 0,
                             max_index: int = 1500,
                             attention_mask_threshold: float = 0.1,
-                            show_graphviz_attrs: bool = False,
-                            show_graphviz_saved: bool = False) -> Digraph:
+                            show_torchviz_attrs: bool = False,
+                            show_torchviz_saved: bool = False) -> Digraph:
     rand_input_ids = torch.randint(min_index, max_index, (1, max_size))
     rand_attention_mask = rand_input_ids > attention_mask_threshold
     output = model(rand_input_ids, rand_attention_mask)
-    return make_dot(output, params=dict(model.named_parameters()), show_attrs=show_graphviz_attrs, show_saved=show_graphviz_saved)
+    return make_dot(output, params=dict(model.named_parameters()), show_attrs=show_torchviz_attrs, show_saved=show_torchviz_saved)
